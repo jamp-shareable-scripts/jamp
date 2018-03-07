7b6bb4c1ae043504e4ec806f368dde3b9728825d
jampperson <mdoaixn@posteo.net>
2018-03-07 22:57:18 +0100

    Add random-string command.

---
+++ core/scripts/random-string.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Generates a random string.
+ *
+ * Usage: jamp random-string [options]
+ * 
+ *   --numbers  Use numbers 0-9 in the string
+ *   --letters  Use letters a-zA-Z in the string
+ *   --length=n Generate a string of length n, default 64
+ * 
+ * @todo Fix off by one error in length of output.
+ * @author  jampperson <https://github.com/jampperson>
+ * @license GPL-2.0
+ */
+
+jampUse(['jampEcho']);
+
+$opts = getopt('', ['letters', 'numbers', 'length:']);
+$charPool = '';
+$letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+$numbers = '0123456789';
+
+// If requested, include letters in the string.
+if (isset($opts['letters'])) {
+	$charPool .= $letters;
+}
+
+// If requested, include numbers in the string.
+if (isset($opts['numbers'])) {
+	$charPool .= $numbers;
+}
+
+// If no character types are provided as options, default to numbers and
+// letters.
+if (empty($charPool)) {
+	$charPool .= $numbers . $letters;
+}
+
+// Use the length given in the options; if none is provided, use a length of 64.
+$length = isset($opts['length']) && is_numeric($opts['length']) 
+? (int)$opts['length']
+: 64;
+
+// The max index of the charPool.
+$max = strlen($charPool);
+
+// Randomly add a character to the string, one at a time.
+$output = '';
+for ($i = 0; $i < $length; $i++) {
+	$output .= substr($charPool, random_int(0, $max), 1);
+}
+
+jampEcho($output);
