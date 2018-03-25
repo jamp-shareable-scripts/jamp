7587af88b46ce8320cd15a8d80fd5539c53dfd0f
jampperson <mdoaixn@posteo.net>
2018-03-25 10:59:56 +0200

    Test ini_set

---
+++ core/scripts/test/test-ini_set.php
@@ -0,0 +1,22 @@
+<?php
+
+$testIniName = 'testini_set' . rand(10000,99999) . '.ini';
+$testIniPath = JAMP_CORE_DATA . $testIniName;
+
+if (file_exists($testIniPath)) {
+	throw new Error("File unexpectedly exists: $testIniPath");
+}
+
+touch($testIniPath);
+
+$pathArg = escapeshellarg($testIniPath);
+
+exec("jamp ini_set -f $pathArg anoption anoptionvalue");
+
+$contents = file_get_contents($testIniPath);
+
+$expected = PHP_EOL . '; Value added via jamp' . PHP_EOL . 'anoption = '
+. 'anoptionvalue' . PHP_EOL;
+
+test($expected === $contents, 'Option should be added.');
+
