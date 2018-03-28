12f6bfdf38090c0b4c99878c9fc6c83abffdfe8c
jampperson <mdoaixn@posteo.net>
2018-03-28 15:47:35 +0200

    Add strtolower script

---
+++ core/scripts/strtolower.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Makes input lowercase.
+ * 
+ * Works as a wrapper for PHP's strtolower function.
+ * 
+ * Usage: jamp strtolower <input>
+ * 
+ * @author  jamp-shareable-scripts <https://github.com/jamp-shareable-scripts>
+ * @license GPL-2.0
+ */
+
+echo strtolower($argv[1]);
