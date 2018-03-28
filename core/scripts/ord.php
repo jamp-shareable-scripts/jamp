4fe467ef9b32362bf049c557ea051e22ae036a1f
jampperson <mdoaixn@posteo.net>
2018-03-28 16:17:43 +0200

    Add ord script

---
+++ core/scripts/ord.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Get the integer representation of a character.
+ * 
+ * Works as a wrapper for PHP's ord function.
+ * 
+ * Usage: jamp ord <character>
+ * 
+ * @author  jamp-shareable-scripts <https://github.com/jamp-shareable-scripts>
+ * @license GPL-2.0
+ */
+
+echo ord($argv[1][0]);
