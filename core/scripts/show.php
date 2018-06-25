3f82d47f84c905bf01b47caa2ac9e787ffb8a7fd
jampperson <mdoaixn@posteo.net>
2018-06-25 18:57:04 +0200

    was better the first time"

--- core/scripts/show.php
+++ core/scripts/show.php
@@ -13,9 +13,9 @@ jampUse('jampEcho');
 $local = is_dir(JAMP_BASE . 'local') ? new FilesystemIterator(
 	JAMP_BASE . 'local' . DIRECTORY_SEPARATOR . 'scripts',
 	FilesystemIterator::SKIP_DOTS | FilesystemIterator::NEW_CURRENT_AND_KEY
-) : [];
+) : false;
 
-if (count($local)) {
+if ($local) {
 	echo 'Local scripts' . PHP_EOL;
 	foreach ($local as $file => $info) {
 		if (strtolower($info->getExtension()) === 'php') {
@@ -38,9 +38,9 @@ foreach ($core as $file => $info) {
 $installed = is_dir(JAMP_BASE . 'installed') ? new FilesystemIterator(
 	JAMP_BASE . 'installed',
 	FilesystemIterator::SKIP_DOTS
-) : [];
+) : false;
 
-if (count($installed)) {
+if ($installed) {
 	echo PHP_EOL . 'Installed scripts' . PHP_EOL;
 	foreach ($installed as $subdirpath) {
 		if (!is_dir($subdirpath)) {
