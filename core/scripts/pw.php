54a6b5c2c649365ca46ad3268c2d6ec03790256b
jamp-shareable-scripts <mdoaixn@posteo.net>
2019-10-30 16:35:15 +0100

    Ability to generate alphanum passwords

--- core/scripts/pw.php
+++ core/scripts/pw.php
@@ -6,6 +6,8 @@
  * Usage: jamp pw [-l <length>|--length <length>]
  * 
  *   -l,--length The length of the password to generate. Default 20.
+ *   -t,--type   The type of characters for the password: 'all' or
+ *               'alphanumeric' or 'an' (alphanumeric shorthand). Default 'all'.
  * 
  * Note that it can be helpful to pass the output straight to the clipboard. For
  * example, `jamp pw | clip` (using the clip program on Windows).
@@ -14,10 +16,16 @@
  * @license GPL-2.0
  */
 
-$opts = getopt('l:', ['length:']);
+$opts = getopt('t:l:', ['length:','type:']);
 
-$chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,./;\''
-. '[]\`-=~!@#$%^&*()_';
+$chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+$type = empty($opts['type']) ? 
+((empty($opts['t'])) ? 20 : $opts['t']) : $opts['type'];
+
+if ($type !== 'alphanumeric' && $type !== 'an') {
+    $chars .= ',./;\'[]\`-=~!@#$%^&*()_';
+}
 
 $length = empty($opts['length']) ? 
 ((empty($opts['l'])) ? 20 : $opts['l']) : $opts['length'];
