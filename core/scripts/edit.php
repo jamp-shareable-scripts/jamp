c2e5c8234ff3f5a73a0a0d7169863e13053146b0
jamp-shareable-scripts <mdoaixn@posteo.net>
2019-10-30 16:35:43 +0100

    Search for installed scripts

--- core/scripts/edit.php
+++ core/scripts/edit.php
@@ -21,27 +21,39 @@ if (!$name) {
 	passthru('jamp usage edit');
 }
 
-$scriptName = substr_compare($name, '.php', -4) === 0
-? $name
-: ($name . '.php');
+$scriptFileName = substr_compare($name, '.php', -4) === 0
+? $name : ($name . '.php');
+$scriptName = substr($scriptFileName, 0, strlen($scriptFileName) - 4);
 
 // Try to open a local version of the script.
 $assumedLocalPath = dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'local'
-. DIRECTORY_SEPARATOR . 'scripts' . DIRECTORY_SEPARATOR . $scriptName;
+. DIRECTORY_SEPARATOR . 'scripts' . DIRECTORY_SEPARATOR . $scriptFileName;
 if (is_file($assumedLocalPath)) {
 	openFile($assumedLocalPath, $printPath);
 	exit();
 }
 
 // Try to open the core version of the script.
-$assumedCorePath = __DIR__ . DIRECTORY_SEPARATOR . $scriptName;
+$assumedCorePath = __DIR__ . DIRECTORY_SEPARATOR . $scriptFileName;
 if (is_file($assumedCorePath)) {
 	openFile($assumedCorePath, $printPath);
 	exit();
 }
 
-throw new Error("Could not find $scriptName. Searched:" . PHP_EOL
-	. "  $assumedLocalPath" . PHP_EOL . "  $assumedCorePath" . PHP_EOL);	
+// Try to open an installed script
+$assumedInstallPath = dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR
+. 'installed' . DIRECTORY_SEPARATOR . "jamp-$scriptName" . DIRECTORY_SEPARATOR
+. 'scripts' . DIRECTORY_SEPARATOR . $scriptFileName;
+if (is_file($assumedInstallPath)) {
+	jampEcho('WARNING: Editing an installed script. Changes will be overridden '
+	. 'if when the script is updated. Save as a local script to prevent this.');
+	openFile($assumedInstallPath, $printPath);
+	exit();
+}
+
+throw new Error("Could not find $scriptFileName. Searched:" . PHP_EOL
+	. "  $assumedLocalPath" . PHP_EOL . "  $assumedCorePath" . PHP_EOL
+	. "  $assumedInstallPath" . PHP_EOL);	
 
 function openFile($path, $printPath) {
 	if ($printPath) {
