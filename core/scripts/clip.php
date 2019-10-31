1124115b8cdb9275c64966712ff39b022748f2a6
jamp-shareable-scripts <mdoaixn@posteo.net>
2019-10-31 14:33:11 +0100

    Add clip function

---
+++ core/scripts/clip.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Copies data to clipboard. Currently only supports Windows.
+ *
+ * Usage: <cmd generating output> | jamp clip
+ * 
+ * WARNING: performs a trim on the input piped into it.
+ * WARNING: only intended for single-line input.
+ */
+
+jampUse('jampIsWindows');
+
+if (!jampIsWindows()) {
+    throw new Exception('jamp clip currently only works on Windows');
+}
+
+$contentForClipboard = str_replace(
+    '\'', '\'\'', trim(stream_get_contents(STDIN))
+);
+
+$cmd = 'powershell -Command  "& {echo \'' . $contentForClipboard
+    . '\' | Set-Clipboard -Value {$_.Trim()}}"';
+exec($cmd);
